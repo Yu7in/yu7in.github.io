e088714b0972f34c00517a7fcc1eca81e3e75aa3
Yu7in <38241854+Yu7in@users.noreply.github.com>
2021-06-29 02:23:57 +0900

    Add files via upload

---
+++ Algorithm/BOJ/BOJ_14681번_사분면 고르기.java
@@ -0,0 +1,25 @@
+// BOJ_14681번_사분면 고르기
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+
+		int answer = 0;
+		if (x > 0 && y > 0) {
+			answer = 1;
+		} else if (x < 0 && y > 0) {
+			answer = 2;
+		} else if (x < 0 && y < 0) {
+			answer = 3;
+		} else {
+			answer = 4;
+		}
+
+		System.out.println(answer);
+	}
+}
