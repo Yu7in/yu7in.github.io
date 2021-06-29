c6dca7987a2639d87e0292d092be57bdf0462333
Yu7in <38241854+Yu7in@users.noreply.github.com>
2021-06-29 19:54:33 +0900

    Add files via upload

---
+++ Algorithm/BOJ/BOJ_11654번_아스키 코드.java
@@ -0,0 +1,13 @@
+// BOJ_11654번_아스키 코드
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int ch = br.readLine().charAt(0);
+		System.out.println(ch);
+	}
+}
