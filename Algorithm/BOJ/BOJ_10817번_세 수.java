9958d52a62830630f7baef37f2d5efc72603ca37
Yu7in <38241854+Yu7in@users.noreply.github.com>
2021-06-29 19:15:06 +0900

    Add files via upload

---
+++ Algorithm/BOJ/BOJ_10817번_세 수.java
@@ -0,0 +1,21 @@
+// BOJ_10817번_세 수
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tokens = new StringTokenizer(br.readLine());
+
+		int[] arr = new int[3];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(tokens.nextToken());
+		}
+
+		Arrays.sort(arr);
+		System.out.println(arr[1]);
+	}
+}
