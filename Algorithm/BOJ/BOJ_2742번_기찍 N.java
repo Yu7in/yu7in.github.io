cc5b12de7526cc971348862727947af902348ae8
Yu7in <38241854+Yu7in@users.noreply.github.com>
2021-06-29 19:42:48 +0900

    Add files via upload

---
+++ Algorithm/BOJ/BOJ_2742번_기찍 N.java
@@ -0,0 +1,17 @@
+// BOJ_2742번_기찍 N
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		while (N > 0) {
+			sb.append(N--).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
