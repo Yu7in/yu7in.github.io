69cada8eea1f89e81d9952f988d39e8186cc9625
Yu7in <38241854+Yu7in@users.noreply.github.com>
2021-07-02 23:54:57 +0900

    Add files via upload

---
+++ Algorithm/BOJ/BOJ_10430번_나머지.java
@@ -0,0 +1,21 @@
+// BOJ_10430번_나머지
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tokens = new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(tokens.nextToken());
+		int B = Integer.parseInt(tokens.nextToken());
+		int C = Integer.parseInt(tokens.nextToken());
+
+		System.out.println((A + B) % C);
+		System.out.println(((A % C) + (B % C)) % C);
+		System.out.println((A * B) % C);
+		System.out.println(((A % C) * (B % C)) % C);
+	}
+}
