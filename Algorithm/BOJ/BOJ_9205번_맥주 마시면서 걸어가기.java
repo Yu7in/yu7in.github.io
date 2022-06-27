309d6dde726c4bd876bf25b04cb1a305acb7be40
Yu7in <38241854+Yu7in@users.noreply.github.com>
2022-06-27 23:36:50 +0900

    Add files via upload

--- Algorithm/BOJ/BOJ_9205번_맥주 마시면서 걸어가기.java
+++ Algorithm/BOJ/BOJ_9205번_맥주 마시면서 걸어가기.java
@@ -16,6 +16,7 @@ public class Main {
 		for (int t = 0; t < T; t++) {
 			n = Integer.parseInt(br.readLine());
 
+			// arr : 출발지(상근이집), n개의 편의점, 도착지(페스티벌) 총 n+2개의 위치 좌표 저장 
 			arr = new ArrayList<int[]>();
 			StringTokenizer tokens = new StringTokenizer(br.readLine());
 			startX = Integer.parseInt(tokens.nextToken());
@@ -34,6 +35,8 @@ public class Main {
 			endY = Integer.parseInt(tokens.nextToken());
 			arr.add(new int[] { endX, endY });
 
+			// station : arr에 저장된 위치들 사이의 거리를 저장 (station[i][j]는 arr.get(i)와 arr.get(j) 사이의 거리 )
+			// visited : arr.get(i)를 방문했는지 여부. 방문했으면 visited[i] = true
 			station = new int[n + 2][n + 2];
 			visited = new boolean[n + 2];
 			for (int i = 0; i < arr.size() - 1; i++) {
@@ -44,12 +47,14 @@ public class Main {
 				}
 			}
 
+			// BFS 사용 
 			Queue<Integer> q = new LinkedList<>();
-			q.add(0);
+			q.add(0); // 출발지 위치부터 시작 
 			while (!q.isEmpty()) {
 				int temp = q.poll();
 				for (int i = 0; i < n + 2; i++) {
 					if (temp != i && !visited[i]) {
+						// 하나의 위치에서 다른 위치로 이동 가능한 경우:  맥주 20병 * 한 병당 가능한 거리 50미터 
 						if (station[temp][i] <= 50 * 20) {
 							visited[i] = true;
 							q.add(i);
@@ -58,6 +63,7 @@ public class Main {
 				}
 			}
 
+			// 방문 했을 시 happy, 실패 시 sad
 			if (visited[n + 1]) {
 				sb.append("happy");
 			} else {
